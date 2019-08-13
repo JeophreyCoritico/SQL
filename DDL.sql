9a1a72a34df5d93404660ccbe8d9c1d47e2b6f6a
JEOPHREY CORITICO <102575814@student.swin.edu.au>
2019-08-13 10:28:10 +1000

    Task 2 Complete

--- DDL.sql
+++ DDL.sql
@@ -5,45 +5,52 @@ drop table TourPRAC
 
 
 Create table TourPRAC (
-    TourName NVARCHAR(5) NOT NULL,
-    Description NVARCHAR(100) NOT NULL,
-    Primary Key (TourName));
+    TourName NVARCHAR(100),
+    Description NVARCHAR(500),
+    Primary Key (TourName),
+    check(TourName in ('North', 'South')));
 
 Create table ClientPRAC (
-    ClientID NUMERIC(1),
-    Surname NVARCHAR(50) NOT NULL,
-    GivenName NVARCHAR(50) NOT NULL,
-    Gender NVARCHAR(1) NOT NULL,
+    ClientID INT,
+    Surname NVARCHAR(100) NOT NULL,
+    GivenName NVARCHAR(100) NOT NULL,
+    Gender NVARCHAR(1) NULL,
     Primary Key (ClientID),
-    check(Gender in ('M', 'F')));
+    check(Gender in ('M', 'F', 'I')));
 
 Create table EventPRAC (
-    TourName NVARCHAR(5) NOT NULL,
-    EventMonth NVARCHAR(3) NOT NULL,
-    EventDay NUMERIC(1),
-    EventYear NUMERIC(4),
-    EventFee NUMERIC(3) NOT NULL,
+    TourName NVARCHAR(100),
+    EventMonth NVARCHAR(3),
+    EventDay INT,
+    EventYear INT,
+    EventFee MONEY NOT NULL,
     Primary Key (EventMonth, EventDay, EventYear, TourName),
-    FOREIGN Key (TourName) references TourPRAC);
+    FOREIGN Key (TourName) references TourPRAC,
+    check(EventMonth in ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')),
+    check(EventDay >= 1 and EventDay <= 31),
+    check(EventYear = 4),
+    check(EventFee >0));
 
 Create table BookingPRAC (
-    ClientID NUMERIC(1),
-    TourName NVARCHAR(5) NOT NULL,
-    EventMonth NVARCHAR(3) NOT NULL,
-    EventDay NUMERIC(1),
-    EventYear NUMERIC(4),
-    Payment NUMERIC(3),
-    DateBooked DATE,
+    ClientID INT,
+    TourName NVARCHAR(100),
+    EventMonth NVARCHAR(3),
+    EventDay INT,
+    EventYear INT,
+    Payment MONEY NULL,
+    DateBooked DATE NOT NULL,
     Primary Key (ClientID, TourName, EventMonth, EventDay, EventYear),
     FOREIGN Key (EventMonth, EventDay, EventYear, TourName) references EventPRAC,
-    FOREIGN Key (ClientID) references ClientPRAC);
+    FOREIGN Key (ClientID) references ClientPRAC,
+    check(EventMonth in ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')),
+    check(EventDay >= 1 and EventDay <= 31),
+    check(EventYear = 4));
 
 
 
 
 SELECT table_catalog [database], table_schema [schema], table_name name, table_type type
 FROM INFORMATION_SCHEMA.TABLES
---WHERE table_name = 'EventPRAC'
 WHERE table_name LIKE '%PRAC'
 GO
 
